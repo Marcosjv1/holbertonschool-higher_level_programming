535ab7a3d0a2d535e5c8ecaa46420cf1c036446b
Marcosjv1 <marcos.javier.vidal@gmail.com>
2026-04-24 20:06:51 -0600

    Create database and user with permissions

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Crea base de datos hbth_0d_2 y user_0d_2
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
